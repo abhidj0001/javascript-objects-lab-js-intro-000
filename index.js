542aacb9972fd64fd00fdb27fec00ce24c3f8395
Abhishek Jadhav <abhidj0001@gmail.com>
2019-08-21 12:44:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function deleteFromObjectByKey(object,key) {
   return newObj;
 }
 
-function destructivelyDeletefromObjectByKey(object,key) {
+function destructivelyDeleteFromObjectByKey(object,key) {
   delete object[key];
   return object;
 }
